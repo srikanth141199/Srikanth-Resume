1be990545d9b166d8660c720e4c9f69285d95bf7
srikanth141199 <kolleparasrikanth@gmailcom>
2023-10-01 23:08:29 +0530

    portfolio

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,14 +9,6 @@ const ProjectCard = ({url, img, github, title, text}) => {
         <div className="capitalize p-8">
             <h2 className="text-xl tracking-wide font-medium ">{title}</h2>
             <p className="text-slate-700 mt-4 leading-loose">{text}</p>
-            <div className="flex gap-4 mt-4">
-                <a href={url}>
-                <TbWorldWww className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-                </a>
-                <a href={github}>
-                <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-                </a>
-            </div>
         </div>
     </article>
   )
