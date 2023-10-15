a6067c13188f3a4045af369ce78361f492373462
srikanth141199 <kolleparasrikanth@gmailcom>
2023-10-15 22:12:35 +0530

    social links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <h1 className='text-7xl font-bold tracking-bolder'>Hi! I'm Srikanth</h1>
                 <h3 className='mt-4 text-3xl capitalize text-slate-700 tracking-wide'>Front-End Developer</h3>
                 <h3 className='mt-4 text-2xl capitalize text-slate-700 tracking-wide'>Code with Creativity, Design with Precision!</h3>
-                <div className=" flex items-left py-4 gap-x-4 ">
+                <div className="flex flex-col sm:flex-row items-left py-4 gap-x-4 ">
                     <a href={`tel:9492622824`} className='flex gap-2 hover:text-sky-600 hover:scale-105 duration-300'><FiPhoneCall className='mt-1'/>9492622824</a>
                     <a target="_blank" href={`mailto:kolleparasrikanth@gmail.com`} className='flex gap-2 hover:text-sky-600 hover:scale-105 duration-300 '><HiOutlineMail className='mt-1  duration-300'/>kolleparasrikanth@gmail.com</a>
                     <a href="https://github.com/srikanth141199"  target="_blank">
